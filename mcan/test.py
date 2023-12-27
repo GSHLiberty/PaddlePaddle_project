6a2f17defd13870508dc50a585be8eae141f0c27
GSHLiberty <103178004+GSHLiberty@users.noreply.github.com>
2023-12-27 11:02:32 +0800

    Delete mcan/test.py

--- mcan/test.py
+++
@@ -1,7 +0,0 @@
-# from core.model.net import Net
-
-# net = Net(0,1,2,3)
-# print(net.state_dict().keys())
-import torch
-pretrained_dict = torch.load("/home/gsh/paddle_project/mcan/ckpts/ckpt_small/epoch13.pkl") #读取存储的数据
-print(pretrained_dict["state_dict"].keys())
